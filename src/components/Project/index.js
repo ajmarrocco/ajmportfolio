4973f811e43d63a5e603f4c1ad71a4968a5b18f1
ajmarrocco <ajmarrocco@gmail.com>
2022-05-08 18:36:46 -0400

    add projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import photo from "../../assets/small/0.png"; 
+import React, { useState } from 'react';
 
 function Project(props) {
-    const currentCategory = {
-        name: "Gym Quest",
-        description:
-            `Collaborative full stack workout social media platform where 
-            users can view workouts based on category and comment on individual workouts`,
-    };
+    const [projects] = useState([
+        {
+            name: "Gym Quest",
+            description:
+                `Collaborative full stack workout social media platform where 
+                users can view workouts based on category and comment on individual workouts`
+        },
+        {
+            name: "Space Sounds!",
+            description:
+                `Space missions from companies such as NASA, SpaceX, Virgin Galactic and Blue 
+                Origin has made the desire to go to space increase exponentially over the past 
+                couple of years. We would like to make that dream realistic for everyone!`
+        },
+        {
+            name: "Shop Shop",
+            description:
+                `This project take the e-commerce platform replaces its implementation to handle 
+                complex state management using Redux, the open-source JavaScript library that it 
+                emulates.`
+        },
+        {
+            name: "Deep Thoughts",
+            description:
+                `The application is a social media application called "Deep Thoughts," where users 
+                can create an account, post their thoughts for others to see, and interact with other 
+                users through these thoughts. This is accomplished by using a MERN application. Feel free 
+                to contribute your own thoughts!`
+        },
+        {
+            name: "Keepin Up With My Budget!",
+            description:
+                `A budget tracker that uses a NoSQL database to enable a website to handle 
+                large amounts of data`
+        },
+        {
+            name: "Techs Marks the Spot",
+            description:
+                `A webpage that allows users to create an acccount. Then the user is able to view, create, 
+                update and delete posts. The user is also allowed to comment on their or another users posts.`,
+        }
+
+    ]);
+
+    const [currentProject, setCurrentProject] = useState();
+
     return (
-        <section>
-            <h1>{currentCategory.name}</h1>
-            <p>{currentCategory.name}</p>
-            <div>
-                <img
-                    src={photo}
-                />
+        <div>
+            <h2>
+                Portfolio
+            </h2>
+            <div className="flex-row">
+                {projects.map((image, i) => (
+                    <img
+                    src={require(`../../assets/small/${i}.png`)}
+                    alt={image.name}
+                    className="img-thumbnail mx-1"
+                    key={image.name}
+                    />
+                ))}
             </div>
-        </section>
+        </div>
     );
 }
 export default Project;
