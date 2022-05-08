321fe20e4d8cd0b5ac14fb9252ef317ed91b77c4
ajmarrocco <ajmarrocco@gmail.com>
2022-05-08 11:26:32 -0400

    contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header></Header>
       <About></About>
+      <Contact></Contact>
       <Footer></Footer>
     </div>
   );
