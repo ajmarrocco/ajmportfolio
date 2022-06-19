f9b3a4eaf43bb334e5f5133639cb17b8911ffbce
ajmarrocco <ajmarrocco@gmail.com>
2022-06-19 11:48:39 -0400

    add frameworks and tools

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,36 +1,79 @@
 import React, { useState } from 'react';
 import downloadResume from '../../assets/pdf/Anthony Marrocco Resume.pdf';
 
-function Resume(props) {
+function Resume() {
 
-    const [skills] = useState(['JavaScript', 'Java', 'C++', 'HTML', 'CSS','Node', 'SQL', 'NoSQL', 'MongoDB', 'React','Express']);
-
+    const [languages] = useState(['JavaScript', 'Java', 'C++', 'C#', 'Python','HTML', 'CSS','Node.js', 'SQL', 'NoSQL', 'MongoDB', 'MySQL']);
+    const [tools] = useState(['Git','GitHub','GitLab','Insomnia','AWS', 'MongoDB Atlas']);
+    const [frameworks] = useState(['React','Express','Sequelize', 'Mongoose','Bootstrap']);
     return (
         <div>
-            <h2>
+            <h2 className='resume-title'>
                 Resume
             </h2>
             <a                  
-                className="App-link"
+                className="App-link resume-link"
                 href={downloadResume}
                 target="_blank"
                 rel="noopener noreferrer">
                     <h3>Download Resume here!</h3>
             </a>
-            <ul> 
-                <h3>
-                    Proficiences:
+            <ul className='language-list'> 
+                <h3 style={{display: 'inline'}}>
+                    Languages:
                 </h3>
-                {skills.map(skill => (
+                {languages.map(language => (
                 <li
+                    style={{display: 'inline'}}
                     className={`mx-1`}
-                    key={skill}
+                    key={language}
                 >
-                    {skill}
+                    {language}
                 </li>
                 ))}
             </ul>
-
+            <ul className='tool-list'> 
+                <h3 style={{display: 'inline'}}>
+                    Tools:
+                </h3>
+                {tools.map(tool => (
+                <li
+                    style={{display: 'inline'}}
+                    className={`mx-1`}
+                    key={tool}
+                >
+                    {tool}
+                </li>
+                ))}
+            </ul>
+            <ul className='framework-list'> 
+                <h3 style={{display: 'inline'}}>
+                    Frameworks:
+                </h3>
+                {frameworks.map(framework => (
+                <li
+                    style={{display: 'inline'}}
+                    className={`mx-1`}
+                    key={framework}
+                >
+                    {framework}
+                </li>
+                ))}
+            </ul>
+            <p>
+                <span className = "icon" title = 'HTML 5' data-tooltip = "HTML 5"><i className="fab fa-html5"></i></span>{" "}
+                <span className = "icon" title = 'CSS' data-tooltip = 'CSS'><i className="fab fa-css3"></i></span>{" "}
+                <span className = "icon" title = 'JavaScript' data-tooltip = 'JavaScript'><i className="fab fa-js"></i></span>{" "}
+                <span className = "icon" title = 'Java' data-tooltip = 'Java'><i className="fab fa-java"></i></span>{" "}
+                <span className = "icon" title = 'Node.js' data-tooltip = 'Node.js'><i className="fab fa-node-js"></i></span>{" "}
+                <span className = "icon" title = 'npm' data-tooltip = 'npm'><i className="fab fa-npm"></i></span>{" "}
+                <span className = "icon" title = 'Express.js' data-tooltip = 'Express.js' style={{fontFamily: "helvetica neue,open sans,sans-serif"}}>ex</span>{" "}
+                <span className = "icon" title = 'SQL' data-tooltip = 'SQL'><i className="fas fa-database"></i></span>{" "}
+                <span className = "icon" title = 'React' data-tooltip = 'React'><i className="fab fa-react"></i></span>{" "}
+                <span className = "icon" title = 'Python' data-tooltip = 'Python'><i className="fab fa-python"></i></span>{" "}
+                <span className = "icon" title = 'AWS' data-tooltip = 'Amazon Web Services'><i className="fab fa-aws"></i></span>{" "}
+                <span className = "icon" title = 'GitHub' data-tooltip = 'GitHub'><i className="fab fa-github-square"></i></span>
+            </p>
         </div>
     );
 }
